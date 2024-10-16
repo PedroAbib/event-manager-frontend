39f3885aaa4be96ef10534985eb50ad147a1ede1
pedro <paulo-ppaa@hotmail.com>
2024-10-16 14:10:04 -0300

    feat: creating TopBar component

---
+++ src/components/navigation/TopBar.tsx
@@ -0,0 +1,13 @@
+import '../../styles/navigation/TopBar.css'
+
+function TopBar() {
+    return(
+        <div className='top-bar'>
+            <div className='title-box'>
+                <h1 className='title'>(Insert title here)</h1>
+            </div>
+        </div>
+    )
+}
+
+export default TopBar;
