ab906f1a4a05211d7c5a20e179278f72fc43181c
pedro <paulo-ppaa@hotmail.com>
2024-10-17 11:03:44 -0300

    refactor: now SideBar receives the NavButton label to define the activePage, also it controls the active NavButton

--- src/components/navigation/SideBar.tsx
+++ src/components/navigation/SideBar.tsx
@@ -1,29 +1,46 @@
+import { useState } from 'react';
 import logo from '../../assets/logo.png';
 import '../../styles/navigation/SideBar.css'
-import HomeIcon from '../../assets/icons/home-icon.svg';
-import EventIcon from '../../assets/icons/event-icon.svg';
-import RegistrantsIcon from '../../assets/icons/registrants-icon.svg';
 import NavButton from '../buttons/NavButton';
+import { LuHome, LuUsers, LuCalendarDays } from "react-icons/lu";
 
-function SideBar() {
+interface SideBarProps {
+    activePage: string;
+    setActivePage: (page: string) => void;
+}
+
+const SideBar: React.FC<SideBarProps> = ({ activePage, setActivePage }) => {
+
+    const handleButtonClick = (page: string) => {
+        setActivePage(page);
+    }
+
+    const iconSize = 18;
+
     return (
         <div>
             <div className='logo-box'>
                 <img src={logo}/>
             </div>
 
-            <div className='#'>
+            <div>
                 <NavButton
-                    icon={HomeIcon}
+                    icon={<LuHome size={iconSize}/>}
                     label='Home'
+                    isActive={activePage === 'Home' ? true : false}
+                    onClick={() => handleButtonClick('Home')}
                 />
                 <NavButton
-                    icon={RegistrantsIcon}
+                    icon={<LuUsers size={iconSize}/>}
                     label='Registrants'
+                    isActive={activePage === 'Registrants' ? true : false}
+                    onClick={() => handleButtonClick('Registrants')}
                 />
                 <NavButton
-                    icon={EventIcon}
+                    icon={<LuCalendarDays size={iconSize}/>}
                     label='Events'
+                    isActive={activePage === 'Events' ? true : false}
+                    onClick={() => handleButtonClick('Events')}
                 />
                 
             </div>
