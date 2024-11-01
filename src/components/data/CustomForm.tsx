40416fb73a4d13f1d4ab0ba7dded38cf5d5be1ba
pedro <paulo-ppaa@hotmail.com>
2024-11-01 16:09:45 -0300

    refactor: CustomForm now renders the input components if they are needed

--- src/components/data/CustomForm.tsx
+++ src/components/data/CustomForm.tsx
@@ -2,38 +2,80 @@ import React from "react";
 import '../../styles/data/CustomForm.css';
 import CustomButton from "../buttons/CustomButton";
 import { useForm } from 'react-hook-form';
+import InputName from "./InputName";
+import InputCPF from "./InputCPF";
+import InputEmail from "./InputEmail";
+import InputPhoneNumber from "./InputPhoneNumber";
 
 interface Field {
     name: string;
     label: string;
-    type: string;
 }
 
 interface CustomFormProps {
     fields: Field[];
+    onCloseModalAfterSubmit: () => void;
 }
 
-const CustomForm: React.FC<CustomFormProps> = ({ fields }) => {
-    const { register, handleSubmit} = useForm();
+const CustomForm: React.FC<CustomFormProps> = ({ fields, onCloseModalAfterSubmit }) => {
+    const { register, handleSubmit, formState: { errors }} = useForm();
 
     const onSubmit = (data : object) => {
-        console.log(data) //////////////////////////////////////////////////
+        console.log(data)
+        onCloseModalAfterSubmit()
     }
 
     return(
-        <div>
-            <form>
+        <div className="form-container-div">
+            <form className="form-container">
                 {fields.map((field, index) => (
-                    <div className='form-card' key={index}>
-                        <label className='form-label'>
-                            {field.label}
-                        </label>
-                        <input
-                            className='form-input'
-                            type={field.type}
-                            placeholder={`Enter ${field.label.toLowerCase()} here...`}
-                            {...register(field.name)}
-                        />
+                    <div key={index}>
+
+                        {errors[field.name] && (
+                            <p className='error-message'>{errors[field.name]?.message as string}</p>
+                        )}
+
+                        {field.name === 'name' && (
+                            <InputName
+                                label={field.label}
+                                register={register(field.name, {
+                                    required: 'Name is required',
+                                    minLength: { value: 3, message: 'Name must be at least 3 characters long'}
+                                })}
+                            />
+                        )}
+                        {field.name === 'tag-name' && (
+                            <InputName
+                                label={field.label}
+                                register={register(field.name)}   
+                            />
+                        )}
+                        {field.name === 'cpf' && (
+                            <InputCPF
+                                label={field.label}
+                                register={register(field.name, {
+                                    required: 'CPF is required',
+                                    minLength: { value: 11, message: 'CPF must be at least 11 characters long'}
+                                }
+                                )}
+                            />
+                        )}
+                        {field.name === 'email' && (
+                            <InputEmail
+                                label={field.label}
+                                register={register(field.name)}
+                            />
+                        )}
+                        {field.name === 'phone-number' && (
+                            <InputPhoneNumber
+                                label={field.label}
+                                register={register(field.name, {
+                                    required: 'Phone number is required',
+                                    minLength: {value: 11, message: 'Phone number must be at least 11 characters long.'}
+                                }
+                                )}    
+                            />
+                        )}
                     </div>
                 ))}
 
