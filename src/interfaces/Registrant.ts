67e8c089ab2ff96e9508bcdd2626ec2c0d0f3365
pedro <paulo-ppaa@hotmail.com>
2025-02-07 17:35:14 -0300

    feature: created Registrant interface

---
+++ src/interfaces/Registrant.ts
@@ -0,0 +1,11 @@
+export interface Registrant {
+    id: string;
+    fullName: string;
+    tagName: string;
+    workField: string;
+    cpf: string;
+    email: string;
+    phoneNumber: string;
+    address: string;
+    postalCode: string;
+}
