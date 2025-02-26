72bf04b0ad2217c5f85d805ae9da70229216f450
pedro <paulo-ppaa@hotmail.com>
2025-02-26 15:58:51 -0300

    refactor: Profile now uses the Registrants hook to delete data and update table

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import EntityDetails from "../components/EntityDetails";
+import CustomButton from "../components/buttons/CustomButton";
+import { useRegistrants } from "../hooks/useRegistrants";
 
 interface ProfileProps {
     entityData: { [key: string]: any };
 }
 
 const Profile: React.FC<ProfileProps> = ({ entityData }) => {
+    const { deleteRegistrant } = useRegistrants();
+
+    const handleDelete = () => {
+        if (window.confirm('Tem certeza que deseja deletar este registro?')) {
+            deleteRegistrant(entityData.id);
+        }
+    }
 
     return(
         <div>
+            <CustomButton
+                label='Delete'
+                onClick={(handleDelete)}
+                color='#ed1e07'
+            />
+
             <EntityDetails entityData={entityData}/>
         </div>
     )
